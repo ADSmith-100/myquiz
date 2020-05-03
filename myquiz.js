bf3d495b24a2710a4b47f4fa8de910eac7bed65d
Adam Smith <coloradoasmith@gmail.com>
2020-05-02 18:46:01 -0600

    adds basic js structure

---
+++ myquiz.js
@@ -0,0 +1,49 @@
+//global variables
+let score = 0;
+let currentQuestion = 0;
+
+//questions array, each ? is an object
+let questions = [
+    {
+        title: "Enter Questions here",
+        answers: ["x","y","z","a"],
+        correct: 1
+
+    },   
+    {
+        title: "Enter Questions here",
+        answers: ["x","y","z","a"],
+        correct: 1
+
+    },   
+    {
+        title: "Enter Questions here",
+        answers: ["x","y","z","a"],
+        correct: 1
+
+    },   
+    {
+        title: "Enter Questions here",
+        answers: ["x","y","z","a"],
+        correct: 1
+
+    },   
+    {
+        title: "Enter Questions here",
+        answers: ["x","y","z","a"],
+        correct: 1
+
+    },   
+]
+
+//event listeners
+
+ 
+
+
+
+//function stubs
+
+
+function showQuestion(){
+   
